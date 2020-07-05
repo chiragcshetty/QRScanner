6ffd5e76a7cb14b9c321b0fb72a5ccf47b0ed13d
Chirag <cshetty2@illinois.edu>
2020-07-05 00:50:21 -0500

    Basic Code

---
+++ app/src/main/java/com/codetoart/android/qrcodescannerandroid/MainActivity.kt
@@ -0,0 +1,45 @@
+package com.codetoart.android.qrcodescannerandroid
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.zxing.integration.android.IntentIntegrator
+import kotlinx.android.synthetic.main.activity_main.*
+import android.net.Uri
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        button_scan_qr_code.setOnClickListener {
+            IntentIntegrator(this).initiateScan()
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        val result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data)
+        if (result != null) {
+            if (result.contents == null) {
+                Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show()
+            } else {
+                Toast.makeText(this, "Scanned: " + result.contents, Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+
+    private fun doSomething() {
+
+        linkBtn.setOnClickListener {
+            val openURL = Intent(android.content.Intent.ACTION_VIEW)
+            openURL.data = Uri.parse("https://www.google.com/")
+            startActivity(openURL)
+        }
+    }
+
+}
